45b5485c5e086a4d8b8b567e648ecd9c0002100b
Jamie Ryan <56791368+MrJamieRyan@users.noreply.github.com>
2020-01-14 14:33:40 +0000

    Successfully fetching country data

--- src/app.js
+++ src/app.js
@@ -4,9 +4,28 @@ document.addEventListener('DOMContentLoaded', () => {
     new Vue ({
         el: '#app',
         data: {
+            allCountries: null,
+
+            selectedCountry: null,
 
         },
+        mounted: function(){
+            this.fetchCountries()
+        },
         methods: {
+            fetchCountries: function(){
+                fetch("https://restcountries.eu/rest/v2/all")
+                .then(response => response.json())
+                .then(jsOb => this.allCountries = jsOb)
+            },
+                showCountryData: function(index){
+                    this.selectedCountry = this.allCountries[index];
+                    console.log(this.selectedCountry);
+                    
+                }
+
+            
+        
 
         }
     })
